1890fcfe4bfcea3357736090bdcf9589c50ddb06
Patricio Almendra <p.almendra.z@gmail.com>
2025-05-03 16:12:38 -0400

    f

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-      "./public/index.html",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: [
+    "./frontend/src/**/*.{js,jsx,ts,tsx}",
+    "./frontend/public/index.html", // Asegúrate de que esta línea esté presente
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
